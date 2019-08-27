043d7624999608276018222179287d240716eeb7
Jai Kishan Kumar <37844679+jaikishan1994@users.noreply.github.com>
2019-08-27 17:00:36 +0530

    Add files via upload

---
+++ ErrorsExceptions.py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+t = int(input())
+for i in range(t):
+    a,b = input().split()
+    try:
+        ans = int(a)/int(b)
+        print(int(ans))
+    except ZeroDivisionError as e:
+        print('Error Code:','integer division or modulo by zero')
+    except ValueError as v:
+        print('Error Code:',v)
